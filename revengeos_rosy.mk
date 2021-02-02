1f4640024e68f6182b12e972b8a3b07ef4e4828b
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-02-02 22:48:58 +0530

    RevengeOS official bringup
    Signed-off-by: Pratyaksh Mehrotra <pratyakshm@protonmail.com>

--- revengeos_rosy.mk
+++ revengeos_rosy.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Pixel customization
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
@@ -21,17 +21,15 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
+PRODUCT_NAME := revengeos_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
+# Build RevengeOS Official
+REVENGEOS_BUILDTYPE := OFFICIAL
 
-# GApps
-TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
